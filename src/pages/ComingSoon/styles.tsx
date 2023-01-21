91c859cb3b94365905dca7ee394ed990fcf91e96
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-01-21 16:23:26 -0500

    adds coming soon screen

---
+++ src/pages/ComingSoon/styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { FlexCenter } from '../../styles';
+import { Screen } from '../Screen';
+
+export const ComingSoonContainer = styled(Screen)`
+  ${FlexCenter}
+`;
+
+export const ContentContainer = styled.div`
+  text-align: center;
+`;
--- src/routers/main.tsx
+++ src/routers/main.tsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from 'react-router';
-import { Dashboard } from '../pages/Dashboard';
-import { NotFound } from '../pages/NotFound';
+import { ComingSoon } from '../pages/ComingSoon';
 import { MainRoutes } from './config';
 
 export const MainRouter = () => {
   return (
     <Routes>
-      <Route path={ MainRoutes.DASHBOARD } element={ <Dashboard /> } />
-      <Route path='*' element={ <NotFound /> } />
+      <Route path={ MainRoutes.DASHBOARD } element={ <ComingSoon /> } />
+      <Route path='*' element={ <ComingSoon /> } />
     </Routes>
   );
 };
